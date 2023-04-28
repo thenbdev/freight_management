c161f29b030d24285d00a301e1451bb991f0b662
dev theNb <dev@thenb.in>
2023-04-28 14:47:33 +0530

    changes

--- freight_management/freight_management/report/track_shipping_order/track_shipping_order.py
+++ freight_management/freight_management/report/track_shipping_order/track_shipping_order.py
@@ -4,6 +4,7 @@
 
 import frappe
 from frappe import _
+from frappe.utils import cstr
 
 
 def execute(filters=None):
@@ -12,22 +13,48 @@ def execute(filters=None):
 
 	columns = get_columns()
 	data = get_data(filters)
-	return columns, data
+
+	report_data = []
+	for row in data:
+		report_row = []
+		for column in columns:
+			cell_value = render_cell(row, column)
+			report_row.append(cell_value)
+		report_data.append(report_row)
+
+	return columns, report_data
+
+
+def render_cell(doc, fieldname, row):
+    print(f"fieldname is {fieldname}")
+    print(f"row is {row}")
+    
+    value = cstr(row.get(fieldname))
+    if fieldname == "my_column":
+        if value == "Value A":
+            return f"<div style='color: red;'>{value}</div>"
+        elif value == "Value B":
+            return f"<div style='color: blue;'>{value}</div>"
+    return value
+
 
 
 def get_columns():
 	return [
-		_("Anchor No") + "::20",
+		_("Anchor No") + "::60",
 		_("Vessel Name") + ":Link/Vessels:120",
-		_("Terminal") + ":Link/Freight Location:200",
+		_("Terminal") + ":Link/Freight Location:120",
 		_("Cargo") + ":Link/Item:100",
-		_("Quantity") + "::120",
-		_("ETA") + ":Date:120",
-		_("ETB") + ":Date:120",
-		_("ETC") + ":Date:120",
-		_("24hr Disch/load rate") + ":Float:60",
-		_("Qty (Remain)") + ":Float:60",
+		_("Quantity") + "::80",
+		_("ETA") + ":Datetime:120",
+		_("ETB") + ":Datetime:120",
+		_("ETC") + ":Datetime:120",
+		_("24hr Disch/load rate") + ":Float:100",
+		_("Qty (Remain)") + ":Float:80",
+		_("Receiver") + ":Cusomer:120",
+		_("Exporters") + ":Cusomer:120",
 		_("Status") + "::120",
+		_("Remarks") + "::120"
 		# _("Company") + ":Link/Company:120",
 	]
 
@@ -52,6 +79,7 @@ def get_data(filters):
 			COALESCE(DS.actual_date_berthing, DS.expected_date_berthing) as ETB,
 			COALESCE(DS.actual_receive_date, DS.expected_receive_date) as ETC,
 			FOL.discharge_rate, FOL.remain_qty,
+			DS.customer, DS.consignee,
 
 			CASE
 				WHEN DS.expected_date_berthing IS NULL THEN 'Unknown'
@@ -62,14 +90,14 @@ def get_data(filters):
 				WHEN DS.actual_date_berthing IS NOT NULL AND DS.actual_receive_date IS NULL AND DS.expected_receive_date <= NOW() THEN 'Overdue Departure'
 				WHEN DS.actual_receive_date IS NOT NULL THEN 'Delivered'
 				ELSE 'Unknown'
-			END AS status
+			END AS status,
+			''
 
 			FROM `tabFreight Order Line` FOL
-			JOIN `tabDirect Shipping` DS
+			RIGHT JOIN `tabDirect Shipping` DS
 			ON FOL.parent = DS.name
-	"""+where_condition
+	"""+where_condition+" order by loading_port"
 
-	print("query is", query)
 	result = frappe.db.sql(query, as_list=1)
 	return [r for r in result]
 
